b25f320972f0d0798376f852a38100159c566290
Fateme Afshari <fateme.afshari1374@gmail.com>
2021-09-20 14:59:45 +0430

    add floating action button

--- lib/src/feature/list/list_page.dart
+++ lib/src/feature/list/list_page.dart
@@ -4,6 +4,10 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
       body: Center(
         child: Text('list'),
       ),
